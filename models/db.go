50906c6c54b1dff66e5f1d6de04b7670c08f9791
Yuichang <zuka0726@gmail.com>
2025-10-26 17:45:04 +0900

    mysql_setup

---
+++ models/db.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv
+	"
+)
+
+func Connect() (*sql.DB, error) {
+	// 環境変数に.envファイルの内容を登録する
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error open .env file")
+		return nil, err
+	}
+
+	// 環境変数に登録されている値を入れる
+	cfg := mysql.Config{
+		User:   os.Getenv("DB_USER"),
+		Passwd: os.Getenv("DB_PASS"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DB_ADDR"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		fmt.Println("DB open Error")
+		return nil, err
+	}
+
+	if db.Ping() != nil {
+		db.Close()
+		fmt.Println("Ping error", err)
+		return nil, err
+	}
+	fmt.Println("connection success!!")
+	return db, nil
+}
---
+++ models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        int64     // db->id
+	Name      string    // db->name
+	Email     string    // db->email
+	CreatedAt time.Time // db->created_at
+	UpdateAt  time.Time // db->updated_at
+}
