c37e979cfbd4ad4ac9289ed60db365dc5d5fd229
Yuichang <zuka0726@gmail.com>
2025-10-29 08:28:47 +0900

    fin insert

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"text/template"
 
+	"github.com/Yuichang/go_user_crud/models"
 	"github.com/Yuichang/go_user_crud/utils"
 )
 
@@ -14,38 +15,47 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func SubmitHandler(w http.ResponseWriter, r *http.Request) {
+// Serverを受け取ってhttp.HandleFuncを返す
+func MakeSubmitHandler(s *models.Server) http.HandlerFunc {
 
-	// 一旦POSTメソッド以外はエラー処理
-	if r.Method != http.MethodPost {
-		http.Error(w, "POSTメソッドで送信してください", http.StatusMethodNotAllowed)
-		return
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// 一旦POSTメソッド以外はエラー処理
+		if r.Method != http.MethodPost {
+			http.Error(w, "POSTメソッドで送信してください", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// フォームの値の取得
+		name := r.FormValue("name")
+		mail := r.FormValue("mail")
+
+		// バリデーションチェック
+		if err_mes := utils.CheckValidation(name, mail); err_mes != "OK" {
+			http.Error(w, err_mes, http.StatusBadRequest)
+			return
+		}
+
+		// データベースに名前とメールアドレスを登録する
+		// 後で名前がユニークかどうか確認する
+		if err := s.InsertUser(name, mail); err != nil {
+			http.Error(w, "DB登録に失敗しました: "+err.Error(), http.StatusInternalServerError)
+		}
+
+		// テンプレートに渡すデータ
+		data := struct {
+			Name string
+			Mail string
+		}{
+			Name: name,
+			Mail: mail,
+		}
+
+		// 結果のページの表示
+		if err := resultTmpl.Execute(w, data); err != nil {
+			http.Error(w, "テンプレートの描画に失敗しました", http.StatusInternalServerError)
+			return
+		}
+
 	}
-
-	// フォームの値の取得
-	name := r.FormValue("name")
-	mail := r.FormValue("mail")
-
-	// バリデーションチェック
-	if err_mes := utils.CheckValidation(name, mail); err_mes != "OK" {
-		http.Error(w, err_mes, http.StatusBadRequest)
-		return
-	}
-
-	// テンプレートに渡すデータ
-	data := struct {
-		Name string
-		Mail string
-	}{
-		Name: name,
-		Mail: mail,
-	}
-
-	// 結果のページの表示
-	if err := resultTmpl.Execute(w, data); err != nil {
-		http.Error(w, "テンプレートの描画に失敗しました", http.StatusInternalServerError)
-		return
-	}
-
-	//models.InsertUser(name, mail)
 }
--- main.go
+++ main.go
@@ -17,12 +17,14 @@ func main() {
 	defer db.Close()
 	log.Println("Connection Success!")
 
+	s := models.NewServer(db)
+
 	// 静的ファイルのハンドラ
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// 各ページのハンドラ
 	http.HandleFunc("/", handlers.IndexHandler)
-	http.HandleFunc("/submit", handlers.SubmitHandler)
+	http.HandleFunc("/submit", handlers.MakeSubmitHandler(s))
 
 	log.Println("Server Start...: http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
